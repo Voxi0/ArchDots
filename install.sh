8ec82e8ab5a2a3197907b6b96708f9ad9ed19d39
Voxi0 <alif2000099@gmail.com>
2024-06-23 15:08:22 +0100

    Bugfix
    Made Sure to Eliminate Any Commands That Changed Directory. This Ensures That no Matter Where The Install Script is, it Can Properly Move All Dotfiles to The Right Locations Without Error of Not Finding Them in The Current Working Directory. Can't Believe I Was Stupid Enough to Miss Such A Major Bug :P

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 # Sync Arch Repos and Update System
 sudo pacman -Syy && sudo pacman -Syu
 
-# Create Basic Directories
-cd ~ && mkdir -p Desktop Downloads
-
 # Move Pacman Config File to System
-sudo mv ./pacman.conf /etc/
+sudo cp pacman.conf /etc/
 
 # Install Packages
 # Base
 sudo pacman -S mesa vulkan-intel wayland xorg-xwayland polkit polkit-gnome qt5-wayland qt6-wayland qt6ct hyprland xdg-desktop-portal-hyprland
-sudo pacman -S papirus-icon-theme hyprpaper waybar rofi libnotify mako udiskie kitty brightnessctl pamixer pavucontrol ufw
+sudo pacman -S papirus-icon-theme hyprpaper waybar rofi-wayland libnotify mako udiskie kitty brightnessctl pamixer pavucontrol ufw
 
 # Apps
 sudo pacman -S nwg-look firefox feh
@@ -24,9 +21,8 @@ sudo pacman -S openssh git gcc cmake wget curl
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install Yay AUR Package Manager
-cd ~/Downloads/
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -sic
-cd .. && rm -rf yay
+git clone https://aur.archlinux.org/yay.git && makepkg -sic
+rm -rf yay
 
 # Install AUR Packages
 yay -S tokyonight-gtk-theme-git bibata-cursor-theme-bin visual-studio-code-bin cava
@@ -36,10 +32,10 @@ sudo pacman -S qt5-quickcontrols2 qt5-graphicaleffects qt5-svg
 sudo pacman -S sddm
 
 # Install SDDM Tokyo Night Theme
-sudo mv ./tokyo-night-sddm /usr/share/sddm/themes/
+sudo cp -r ./tokyo-night-sddm /usr/share/sddm/themes/
 
 # Install GRUB Tokyo Night Theme
-cd ~/Downloads/ && git clone https://github.com/mino29/tokyo-night-grub.git
+git clone https://github.com/mino29/tokyo-night-grub.git
 sudo cp -r tokyo-night-grub/tokyo-night /usr/share/grub/themes/
 rm -rf tokyo-night-grub
 
@@ -52,12 +48,12 @@ git config --global user.email "alif2000099@gmail.com"
 sudo ufw enable && sudo ufw allow ssh && sudo ufw status verbose
 
 # Move Config Files to System
-sudo mv ./grub /etc/default/
+sudo cp ./grub /etc/default/
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-sudo mv ./sddm.conf /etc/
-sudo mv ./index.theme /usr/share/icons/default/
-sudo rm -rf ~/.config && sudo mv ./.config ~/
-sudo mv ./Pictures ./Documents ~/
+sudo cp ./sddm.conf /etc/
+sudo cp ./index.theme /usr/share/icons/default/
+sudo rm -rf ~/.config && sudo cp -r ./.config ~/
+sudo cp -r ./Pictures ~/
 
 # Add User to Groups
 sudo usermod -aG video voxi0
@@ -66,5 +62,5 @@ sudo usermod -aG uucp voxi0
 # System Services
 sudo systemctl enable sddm.service
 
-# Reboot
-reboot
+# End
+echo "\nArchDots Installed! You Should Probably Reboot Your System Now."
