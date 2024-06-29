66a228743d8333d0f96900065b65df690e3d89e1
Voxi0 <alif200099@gmail.com>
2024-06-29 19:36:01 +0100

    Huge Refactor
    Learned A Lot More About Bash Scripting and Used Newfound Knowledge to Refactor Everything to Make Sure It's Organized in A Nice Order. Might Have Some Issues Though But I Will Eliminate Those by Finding The Issues by Running The Script on A VM Very Soon.

--- install.sh
+++ install.sh
@@ -1,64 +1,121 @@
-# Sync Arch Repos and Update System
-sudo pacman -Syy && sudo pacman -Syu
+#!/bin/bash
+# Variables
+base_pkgs="
+    # Base
+    mesa vulkan-intel sddm polkit polkit-gnome libnotify udiskie
 
-# Move Pacman Config File to System
-sudo cp pacman.conf /etc/
+    # Terminal and CLI Utilities
+    kitty brightnessctl pamixer imagemagick neofetch btop acpi unzip neovim cava
 
-# Install Packages
-# Base
-sudo pacman -S mesa vulkan-intel wayland xorg-xwayland polkit polkit-gnome qt5-wayland qt6-wayland qt6ct hyprland xdg-desktop-portal-hyprland
-sudo pacman -S papirus-icon-theme hyprpaper waybar rofi-wayland libnotify mako udiskie kitty brightnessctl pamixer pavucontrol ufw
+    # QT
+    qt5-wayland qt5-quickcontrols2 qt5-graphicaleffects qt5-svg qt6-wayland qt6ct
 
-# Apps
-sudo pacman -S nwg-look firefox feh
+    # Fonts
+    ttf-jetbrains-mono-nerd ttf-cascadia-code-nerd font-manager
 
-# Fonts
-sudo pacman -S ttf-jetbrains-mono-nerd ttf-cascadia-code-nerd font-manager
+    # Themes - GTK, Cursor and Icon
+    tokyonight-gtk-theme-git bibata-cursor-theme-bin papirus-icon-theme"
+wayland_pkgs="
+    wayland xorg-xwayland hyprland xdg-desktop-portal-hyprland waybar rofi-wayland mako grim slurp"
+apps="
+    pavucontrol nwg-look firefox feh visual-studio-code-bin"
+dev_tools="
+    openssh git gcc cmake wget curl"
 
-# CLI Utils
-sudo pacman -S neofetch imagemagick btop acpi unzip grim slurp neovim
+# TUI - Simple Text-User Interface
+function TUI() {
+    # Display A Simple Text-User Interface
+    echo -e "\tArchDots by Voxi0"
+    echo "1) Install"
+    echo "2) Uninstall"
+    read -p "Choice: " choice
+    case $choice in
+        1)
+            install_pkgs
+            ;;
+        2)
+            uninstall_pkgs
+            ;;
+        *)
+            echo "Invalid Option!"
+            echo
+            TUI
+    esac
+}
 
-# Dev Tools
-sudo pacman -S openssh git gcc cmake wget curl
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Install/Uninstall Packages
+function install_pkgs() {
+    # Move Pacman Config File to System, Sync Arch Repos and Update System
+    sudo cp pacman.conf /etc/ && sudo pacman -Syy && sudo pacman -Syu
 
-# Install Yay AUR Package Manager
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -sic && cd ..
-rm -rf yay
+    # Install Yay AUR Package Manager
+    git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -sic && cd ..
+    rm -rf yay
 
-# Install AUR Packages
-yay -S tokyonight-gtk-theme-git bibata-cursor-theme-bin visual-studio-code-bin cava
+    # Install Packages
+    yay -S $base_pkgs $wayland_pkgs $apps $dev_tools
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Install SDDM
-sudo pacman -S qt5-quickcontrols2 qt5-graphicaleffects qt5-svg
-sudo pacman -S sddm
+    # Install Dots
+    install_dots
+}
+function uninstall_pkgs() {
+    # Install Yay AUR Package Manager - Just so The Commands Below Works
+    git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -sic && cd ..
+    rm -rf yay
 
-# Install SDDM Tokyo Night Theme
-sudo cp -r ./TokyoNight-SDDM /usr/share/sddm/themes/
+    # Uninstall Packages
+    yay -Rscun $base_pkgs $wayland_pkgs $apps $dev_tools
+    rustup self uninstall
+}
 
-# Install GRUB Tokyo Night Theme
-git clone https://github.com/mino29/tokyo-night-grub.git
-sudo cp -r tokyo-night-grub/tokyo-night /usr/share/grub/themes/
-rm -rf tokyo-night-grub
+# Install Dotfiles to System - '.config', SDDM, GRUB and Default Icon Config
+function install_dots() {
+    # SDDM and Default Icon Config
+    sudo cp ./.config/index.theme /usr/share/icons/default/
+    sudo cp -r ./TokyoNight-SDDM /usr/share/sddm/themes/ && sudo cp ./.config/sddm.conf /etc/
 
-# Enable Firewall
-sudo ufw enable && sudo ufw allow ssh && sudo ufw status verbose
+    # GRUB - Install GRUB Theme and Move GRUB Config to System
+    git clone https://github.com/mino29/tokyo-night-grub.git && sudo cp -r tokyo-night-grub/tokyo-night /usr/share/grub/themes/
+    rm -rf tokyo-night-grub
+    sudo cp ./.config/grub /etc/default/ && sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-# Move Config Files to System
-sudo cp ./grub /etc/default/ && sudo cp ./sddm.conf /etc/ && sudo cp ./index.theme /usr/share/icons/default/
-sudo rm -rf ~/.config && sudo cp -r ./.config ~/
-sudo cp -r ./Pictures ~/ && sudo cp -r ./Documents ~/
+    # Home - '.config', 'Pictures' and 'Documents' Folders
+    sudo rm -rf ~/.config && sudo cp -r ./.config ~/
+    sudo cp -r ./Pictures ~/ && sudo cp -r ./Documents ~/
 
-# Regenerate Grub Config
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+    # Finish
+    finish
+}
 
-# Add User to Groups
-sudo usermod -aG video voxi0
-sudo usermod -aG uucp voxi0
+# Add User to Groups, Enable System Services and Run Custom Commands
+function finish() {
+    # Enable Firewall
+    sudo ufw enable && sudo ufw allow ssh && sudo ufw status verbose
 
-# System Services
-sudo systemctl enable sddm.service
+    # Add User to Groups
+    sudo usermod -aG video voxi0
+    sudo usermod -aG uucp voxi0
 
-# End
-echo && echo
-echo "ArchDots Installed! You Should Probably Reboot Your System Now."
+    # Enable System Services
+    sudo systemctl enable sddm.service
+
+    # Finish Message
+    echo && echo
+    echo "Would You Like to Reboot Now?"
+    read -p "Y/N: " choice
+    case $choice in
+        'Y')
+            reboot
+            ;;
+        'N')
+            ;;
+        *)
+            echo "Invalid Option! Rebooting in 5 Seconds."
+            sleep 5
+            reboot
+    esac
+}
+
+# Script Entry Point
+TUI
